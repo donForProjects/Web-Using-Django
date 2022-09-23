f3a678050ac3e0e11236c06d9306a3b1d2907c51
donForProjects <comiaforprojects@gmail.com>
2022-09-23 20:01:46 +0800

    First Init

---
+++ mysite/main/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<str:name>', views.index, name = 'index'), #default to index
+]
---
+++ mysite/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+
+# Create your views here.
+
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
+
